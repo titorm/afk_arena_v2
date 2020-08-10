cb64479fb1cbacec4c30406323a89be428131ef2
titoribeiromotter <titoribeiromotter@gmail.com>
2020-08-10 20:28:52 -0300

    First Structure

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: 'AIzaSyB5F_WxKVaQEEFMo6vAM8ylgvdISfsyMso',
+    authDomain: 'afk-arena-v2.firebaseapp.com',
+    databaseURL: 'https://afk-arena-v2.firebaseio.com',
+    projectId: 'afk-arena-v2',
+    storageBucket: 'afk-arena-v2.appspot.com',
+    messagingSenderId: '417976254636',
+    appId: '1:417976254636:web:f1aec7bd905a480a321f09',
+};
+
+export default config;
