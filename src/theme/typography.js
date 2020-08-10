e476bb0d811c0427fd8bc5df938e0ebfe22dd85c
titoribeiromotter <titoribeiromotter@gmail.com>
2020-08-10 20:54:01 -0300

    Typography

---
+++ src/theme/typography.js
@@ -0,0 +1,24 @@
+const fontFamily = {
+    thin: 'WorkSans-Thin',
+    thinItalic: 'WorkSans-ThinItalic',
+    extraLight: 'WorkSans-ExtraLight',
+    extraLightItalic: 'WorkSans-ExtraLightItalic',
+    light: 'WorkSans-Light',
+    lightItalic: 'WorkSans-LightItalic',
+    regular: 'WorkSans-Regular',
+    regularLight: 'WorkSans-RegularItalic',
+    medium: 'WorkSans-Medium',
+    mediumItalic: 'WorkSans-MediumItalic',
+    bold: 'WorkSans-Bold',
+    boldItalic: 'WorkSans-BoldItalic',
+    extraBold: 'WorkSans-ExtraBold',
+    extraBoldItalic: 'WorkSans-ExtraBoldItalic',
+    black: 'WorkSans-Black',
+    blackItalic: 'WorkSans-BlackItalic',
+};
+
+const typography = {
+    ...fontFamily,
+};
+
+export default typography;
