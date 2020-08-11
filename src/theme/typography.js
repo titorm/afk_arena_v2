ccbec3bbbdafbe687b357e6cd256bff4c45119ee
titoribeiromotter <titoribeiromotter@gmail.com>
2020-08-10 22:03:52 -0300

    typography update

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -17,8 +17,49 @@ const fontFamily = {
     blackItalic: 'WorkSans-BlackItalic',
 };
 
+const fontSize = {
+    h1: 96,
+    h2: 60,
+    h3: 48,
+    h4: 34,
+    h5: 24,
+    h6: 20,
+    subtitle1: 16,
+    subtitle2: 14,
+    body1: 16,
+    body2: 14,
+    button: 14,
+    caption: 12,
+    overline: 10,
+};
+
+const letterSpacing = {
+    h1: -1.5,
+    h2: -0.5,
+    h3: 0,
+    h4: 0.25,
+    h5: 0,
+    h6: 0.15,
+    subtitle1: 0.15,
+    subtitle2: 0.1,
+    body1: 0.5,
+    body2: 0.25,
+    button: 1.25,
+    caption: 0.4,
+    overline: 1.5,
+};
+
+const textTransform = {
+    uppercase: 'uppercase',
+    capitalize: 'capitalize',
+    lowecase: 'lowercase',
+};
+
 const typography = {
-    ...fontFamily,
+    fontFamily,
+    textTransform,
+    fontSize,
+    letterSpacing,
 };
 
 export default typography;
