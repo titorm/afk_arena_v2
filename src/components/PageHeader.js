342b40d269a0b4b7ca6c0def66523cb0d5714d9e
Tito <titoribeiromotter@gmail.com>
2021-01-01 12:54:12 -0300

    PageHeader improved

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Defs, Line, LinearGradient, Stop, Svg } from 'react-native-svg';
 
 import colors from '../theme/colors/colors';
 import typography from '../theme/typography';
@@ -11,6 +12,39 @@ function PageHeader(props) {
         <>
             <Text style={styles.h4}>{title}</Text>
             <Text style={styles.subtitle2}>{subtitle}</Text>
+            <View style={styles.gradientContainer}>
+                <Svg
+                    height='2'
+                    width='100'
+                >
+                    <Defs>
+                        <LinearGradient
+                            id='grad'
+                            x1='0'
+                            y1='0'
+                            x2='1'
+                            y2='0'
+                        >
+                            <Stop
+                                offset='0'
+                                stopColor={colors.primary}
+                                stopOpacity='1'
+                            />
+                            <Stop
+                                offset='1'
+                                stopColor={colors.secondary}
+                                stopOpacity='1'
+                            />
+                        </LinearGradient>
+                    </Defs>
+                    <Line
+                        x1='0'
+                        x2='100'
+                        stroke='url(#grad)'
+                        strokeWidth='2'
+                    />
+                </Svg>
+            </View>
         </>
     );
 }
@@ -30,6 +64,11 @@ const styles = StyleSheet.create({
         fontFamily: typography.fontFamily.regular,
         letterSpacing: typography.letterSpacing.h4,
         marginBottom: 8,
+    },
+    gradientContainer: {
+        alignItems: 'center',
+        marginBottom: 16,
+        justifyContent: 'center',
     },
 });
 
